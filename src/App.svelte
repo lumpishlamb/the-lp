c5eec5c90c53ea1a799469367c934eba30afa411
thekp <thekp97@gmail.com>
2020-12-23 23:57:13 +0000

    Firt commit 🥔

---
+++ src/App.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+</script>
+
+<style>
+	header {
+		background-color: #131a21;
+		color: white;
+	}
+	div {
+		background-image: url(https://images-eu.ssl-images-amazon.com/images/G/02/gno/sprites/nav-sprite-global-1x-hm-dsk-reorg._CB405938116_.png);
+		background-position: 0 1000px;
+		background-repeat: repeat-x;
+		background-position: -10px -90px;
+		width: 76px;
+		height: 23px;
+		margin: 0 auto;
+	}
+	nav {
+		background-color: #22303e;
+		width: 100%;
+	}
+	ul {
+		padding: 0px;
+		margin: 0px;
+		display: flex;
+	}
+	li {
+		margin: 8px;
+		list-style: none;
+	}
+</style>
+
+<header>
+	<ul>
+		<li>
+			<div />
+			<span />
+		</li>
+		<li>location</li>
+		<li>search bar</li>
+		<li>account</li>
+		<li>basket</li>
+	</ul>
+	<nav>
+		<ul>
+			<li>Github</li>
+			<li>Strava</li>
+			<li>LinkedIn</li>
+		</ul>
+	</nav>
+</header>
